1c509367fd773fe5faa554645fc96653ee33ede6
Ram Bansal <ramnesh.bansal@gmail.com>
2020-06-08 22:59:07 -0400

    closeby restaurants seed data

---
+++ RestaurantData.js
@@ -0,0 +1,140 @@
+/* eslint-disable */
+const RestaurantsData = {
+  results: [
+    {
+      geometry: {
+        location: {
+          lat: 43.0912873,
+          lng: -79.1231231,
+        },
+      },
+      name: 'Tacontento Mexican Foods',
+      cuisine: 'Mexican',
+      image: require('./assets/cuisine/mexican/mexican.jpg'),
+      place_id: '1',
+      vicinity: 'Silicon Valley',
+    },
+    {
+      geometry: {
+        location: {
+          lat: 43.0912873,
+          lng: -79.1231231,
+        },
+      },
+      name: 'El Takito',
+      cuisine: 'Mexican',
+      opening_hours: {
+        open_now: false,
+      },
+      image: require('./assets/cuisine/mexican/mexican.jpg'),
+      place_id: '2',
+      vicinity: 'Silicon Valley',
+    },
+    {
+      geometry: {
+        location: {
+          lat: 43.0912873,
+          lng: -79.1231231,
+        },
+      },
+      name: "Yo Amo Tacos Bar 'n Grill",
+      cuisine: 'Mexican',
+      image: require('./assets/cuisine/mexican/mexican.jpg'),
+      place_id: '3',
+      vicinity: 'Silicon Valley',
+    },
+    {
+      geometry: {
+        location: {
+          lat: 43.09128730000001,
+          lng: -79.1231231,
+        },
+      },
+      name: "Annie's Grill, Toronto",
+      cuisine: 'Mexican',
+      image: require('./assets/cuisine/mexican/mexican.jpg'),
+      place_id: '4',
+      vicinity: 'Silicon Valley',
+    },
+    {
+      geometry: {
+        location: {
+          lat: 43.0912873,
+          lng: -79.1231231,
+        },
+      },
+      name: 'Comal y Canela',
+      cuisine: 'Mexican',
+      image: require('./assets/cuisine/mexican/mexican.jpg'),
+      place_id: '5',
+      vicinity: 'Silicon Valley',
+    },
+    {
+      geometry: {
+        location: {
+          lat: 43.0912873,
+          lng: -79.1231231,
+        },
+      },
+      name: 'Cali Mexican Grill',
+      cuisine: 'Mexican',
+      image: require('./assets/cuisine/mexican/mexican.jpg'),
+      place_id: '6',
+      vicinity: 'Silicon Valley',
+    },
+    {
+      geometry: {
+        location: {
+          lat: 43.0912873,
+          lng: -79.1231231,
+        },
+      },
+      name: 'Chipotle Mexican Grill',
+      cuisine: 'Mexican',
+      image: require('./assets/cuisine/mexican/mexican.jpg'),
+      place_id: '7',
+      vicinity: 'Silicon Valley',
+    },
+    {
+      geometry: {
+        location: {
+          lat: 43.0912873,
+          lng: -79.1231231,
+        },
+      },
+      name: 'Get & Go Burrito',
+      cuisine: 'Mexican',
+      image: require('./assets/cuisine/mexican/mexican.jpg'),
+      place_id: '8',
+      vicinity: 'Silicon Valley',
+    },
+    {
+      geometry: {
+        location: {
+          lat: 43.0912873,
+          lng: -79.1231231,
+        },
+      },
+      name: "Amigo's Latin Flavor",
+      cuisine: 'Mexican',
+      image: require('./assets/cuisine/mexican/mexican.jpg'),
+      place_id: '9',
+      vicinity: 'Silicon Valley',
+    },
+    {
+      geometry: {
+        location: {
+          lat: 43.0912873,
+          lng: -79.1231231,
+        },
+      },
+      name: 'Mexican Hot Sauce Inc.',
+      cuisine: 'Mexican',
+      image: require('./assets/cuisine/mexican/mexican.jpg'),
+      place_id: '10',
+      vicinity: 'Silicon Valley',
+    },
+  ],
+};
+
+export default RestaurantsData;
---
+++ RestaurantList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 16,
+  },
+  item: {
+    backgroundColor: '#f9f9f9',
+    marginVertical: 8,
+    padding: 10,
+  },
+  sectionHeader: {
+    fontSize: 16,
+    fontWeight: '900',
+  },
+  name: {
+    marginTop: 16,
+    fontSize: 16,
+  },
+  cuisine: {
+    marginTop: 8,
+    fontSize: 12,
+  },
+  vicinity: {
+    marginTop: 8,
+    fontSize: 12,
+  },
+  image: {
+    width: 305,
+    height: 150,
+  },
+});
