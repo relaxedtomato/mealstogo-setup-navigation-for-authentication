ea564841d6d79a52fc93750c2eaad19fdd07fdc9
Ram Bansal <ramnesh.bansal@gmail.com>
2020-06-11 22:57:35 -0400

    favorite restaurant

--- RestaurantList.js
+++ RestaurantList.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Image, SectionList, StyleSheet, Text, View } from 'react-native';
+import {
+  Image,
+  ScrollView,
+  SectionList,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import PropTypes from 'prop-types';
 import RestaurantData from './RestaurantData';
 
 const DATA = [
   {
+    title: 'Favorites',
+    data: [RestaurantData.results.slice(11, 16)], // [[r1, r2, r3]]
+    renderItem: ({ item }) => <FavRestaurant data={item} />,
+  },
+  {
     title: 'Close by',
-    data: RestaurantData.results,
+    data: RestaurantData.results.slice(0, 10), // [r1, r2, r3]
+    renderItem: ({ item }) => <ClosebyRestaurant data={item} />,
   },
 ];
 
@@ -40,9 +53,17 @@ const styles = StyleSheet.create({
     width: 305,
     height: 150,
   },
+  favContainer: {
+    padding: 8,
+    backgroundColor: '#f9f9f9',
+  },
+  favImage: {
+    width: 150,
+    height: 100,
+  },
 });
 
-const Item = ({ data }) => (
+const ClosebyRestaurant = ({ data }) => (
   <View style={styles.item}>
     <Image style={styles.image} source={data.image} />
     <Text style={styles.name}>{data.name}</Text>
@@ -51,7 +72,7 @@ const Item = ({ data }) => (
   </View>
 );
 
-Item.propTypes = {
+ClosebyRestaurant.propTypes = {
   data: PropTypes.shape({
     image: PropTypes.number,
     name: PropTypes.string,
@@ -60,12 +81,22 @@ Item.propTypes = {
   }).isRequired,
 };
 
+const FavRestaurant = ({ data }) => (
+  <ScrollView horizontal>
+    {data.map(item => (
+      <View style={styles.favContainer}>
+        <Image source={item.image} style={styles.favImage} />
+        <Text style={styles.name}>{item.name}</Text>
+      </View>
+    ))}
+  </ScrollView>
+);
+
 const RestaurantList = () => (
   <View style={styles.container}>
     <SectionList
       sections={DATA}
       keyExtractor={item => item.place_id}
-      renderItem={({ item }) => <Item data={item} />}
       renderSectionHeader={({ section: { title } }) => (
         <Text style={styles.sectionHeader}>{title}</Text>
       )}
