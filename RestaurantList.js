bbf101a04f92c05ddd13e91a742312117c183622
Ram Bansal <ramnesh.bansal@gmail.com>
2020-06-13 15:43:44 -0400

    code cleanup

--- RestaurantList.js
+++ RestaurantList.js
@@ -1,31 +1,43 @@
 import React from 'react';
-import {
-  Image,
-  ScrollView,
-  SectionList,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import { Image, SectionList, StyleSheet, Text, View } from 'react-native';
 import PropTypes from 'prop-types';
 import Swiper from 'react-native-swiper';
 
 import RestaurantData from './RestaurantData';
 
+const restaurantPropTypes = PropTypes.shape({
+  image: PropTypes.number,
+  name: PropTypes.string,
+  cuisine: PropTypes.string,
+  vicinity: PropTypes.string,
+});
+
+const renderFavRestaurant = ({ item: restaurants }) => (
+  <FavRestaurant restaurants={restaurants} />
+);
+
+renderFavRestaurant.propTypes = {
+  item: restaurantPropTypes.isRequired,
+};
+
+const renderClosebyRestaurant = ({ item: restaurant }) => (
+  <ClosebyRestaurant restaurant={restaurant} />
+);
+
+renderClosebyRestaurant.propTypes = {
+  item: restaurantPropTypes.isRequired,
+};
+
 const RESTAURANT_DATA = [
   {
     title: 'Favorites',
     data: [RestaurantData.results.slice(11, 16)], // [[r1, r2, r3]]
-    renderItem: ({ item: restaurants }) => (
-      <FavRestaurant restaurants={restaurants} />
-    ),
+    renderItem: renderFavRestaurant,
   },
   {
     title: 'Close by',
     data: RestaurantData.results.slice(0, 10), // [r1, r2, r3]
-    renderItem: ({ item: restaurant }) => (
-      <ClosebyRestaurant restaurant={restaurant} />
-    ),
+    renderItem: renderClosebyRestaurant,
   },
 ];
 
@@ -43,7 +55,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: '900',
   },
-  name: {
+  closebyName: {
     marginTop: 16,
     fontSize: 16,
   },
@@ -67,6 +79,10 @@ const styles = StyleSheet.create({
     width: 150,
     height: 100,
   },
+  favName: {
+    marginTop: 8,
+    fontSize: 16,
+  },
   swiper: {
     height: 160,
   },
@@ -78,19 +94,14 @@ const styles = StyleSheet.create({
 const ClosebyRestaurant = ({ restaurant }) => (
   <View style={styles.closebyContainer}>
     <Image style={styles.closebyImage} source={restaurant.image} />
-    <Text style={styles.name}>{restaurant.name}</Text>
+    <Text style={styles.closebyName}>{restaurant.name}</Text>
     <Text style={styles.cuisine}>{restaurant.cuisine}</Text>
     <Text style={styles.vicinity}>{restaurant.vicinity}</Text>
   </View>
 );
 
 ClosebyRestaurant.propTypes = {
-  restaurant: PropTypes.shape({
-    image: PropTypes.number,
-    name: PropTypes.string,
-    cuisine: PropTypes.string,
-    vicinity: PropTypes.string,
-  }).isRequired,
+  restaurant: restaurantPropTypes.isRequired,
 };
 
 const FavRestaurant = ({ restaurants }) => {
@@ -130,7 +141,7 @@ const FavRestaurant = ({ restaurants }) => {
           <View style={styles.slide} key={firstRestaurant.place_id}>
             <View style={styles.favContainer} key={firstRestaurant.place_id}>
               <Image source={firstRestaurant.image} style={styles.favImage} />
-              <Text style={styles.name}>{firstRestaurant.name}</Text>
+              <Text style={styles.favName}>{firstRestaurant.name}</Text>
             </View>
             {secondRestaurant && (
               <View style={styles.favContainer} key={secondRestaurant.place_id}>
@@ -138,7 +149,7 @@ const FavRestaurant = ({ restaurants }) => {
                   source={secondRestaurant.image}
                   style={styles.favImage}
                 />
-                <Text style={styles.name}>{secondRestaurant.name}</Text>
+                <Text style={styles.favName}>{secondRestaurant.name}</Text>
               </View>
             )}
           </View>
@@ -148,36 +159,9 @@ const FavRestaurant = ({ restaurants }) => {
   );
 };
 
-// (
-//   <Swiper showsPagination={false} style={styles.swiper}>
-//     {/* {restaurants.map(restaurant => (
-//       <View style={styles.favContainer} key={restaurant.place_id}>
-//         <Image source={restaurant.image} style={styles.favImage} />
-//         <Text style={styles.name}>{restaurant.name}</Text>
-//       </View>
-//     ))} */}
-//     <View style={styles.slide}>
-//       <View style={styles.favContainer} key={restaurants[0].place_id}>
-//         <Image source={restaurants[0].image} style={styles.favImage} />
-//         <Text style={styles.name}>{restaurants[0].name}</Text>
-//       </View>
-//       <View style={styles.favContainer} key={restaurants[1].place_id}>
-//         <Image source={restaurants[1].image} style={styles.favImage} />
-//         <Text style={styles.name}>{restaurants[1].name}</Text>
-//       </View>
-//     </View>
-//     <View style={styles.slide}>
-//       <View style={styles.favContainer} key={restaurants[2].place_id}>
-//         <Image source={restaurants[2].image} style={styles.favImage} />
-//         <Text style={styles.name}>{restaurants[2].name}</Text>
-//       </View>
-//       <View style={styles.favContainer} key={restaurants[3].place_id}>
-//         <Image source={restaurants[3].image} style={styles.favImage} />
-//         <Text style={styles.name}>{restaurants[3].name}</Text>
-//       </View>
-//     </View>
-//   </Swiper>
-// );
+FavRestaurant.propTypes = {
+  restaurants: PropTypes.arrayOf(restaurantPropTypes).isRequired,
+};
 
 const RestaurantList = () => (
   <View style={styles.container}>
