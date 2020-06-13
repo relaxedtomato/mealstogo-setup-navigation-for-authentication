17deda0b05527831971e93be61e10c2a83488d6f
Ram Bansal <ramnesh.bansal@gmail.com>
2020-06-13 18:28:52 -0400

    configure module resolver

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,21 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '~/assets': './assets',
+            '~/components': './src/components',
+            '~/scenes': './src/scenes',
+            '~/services': './src/services',
+            '~/styles': './src/styles',
+            '~/utils': './src/utils',
+          },
+        },
+      ],
+    ],
   };
 };
