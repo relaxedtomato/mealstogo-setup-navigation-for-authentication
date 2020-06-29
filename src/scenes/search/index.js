b109526f9aaff33cf6b55dee391cfdf29b10973c
Ram Bansal <ramnesh.bansal@gmail.com>
2020-06-29 11:24:04 -0400

    search text input

--- src/scenes/search/index.js
+++ src/scenes/search/index.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StatusBar, StyleSheet, TextInput } from 'react-native';
+import { Colors, Typography, Spacing } from '~/styles';
+import { TOP_BAR_HEIGHT } from '~/utils/constants';
 
-const Search = () => (
-  <SafeAreaView>
-    <Text>Search</Text>
-  </SafeAreaView>
-);
+const styles = StyleSheet.create({
+  container: {
+    marginTop: StatusBar.currentHeight,
+  },
+  textInput: {
+    ...Typography.bodyText,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: Colors.mediumGray,
+    color: Colors.darkGray,
+    height: TOP_BAR_HEIGHT,
+    paddingLeft: Spacing.medium,
+    paddingVertical: Spacing.medium,
+  },
+});
+
+const Search = () => {
+  const [searchText, onChangeText] = useState('Type a cuisine or place name');
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        onFocus={() => onChangeText('')}
+        onChangeText={onChangeText}
+        value={searchText}
+        style={styles.textInput}
+      />
+    </SafeAreaView>
+  );
+};
 
 export default Search;
