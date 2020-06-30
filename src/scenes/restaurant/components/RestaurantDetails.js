3b9f1e8b1cbcabdae33ddb7683b512ec575b38ad
Ram Bansal <ramnesh.bansal@gmail.com>
2020-06-29 20:47:55 -0400

    exercise code, toggle fav icon

--- src/scenes/restaurant/components/RestaurantDetails.js
+++ src/scenes/restaurant/components/RestaurantDetails.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Image, Text, StyleSheet, TouchableOpacity, View } from 'react-native';
 
 import { Colors, Typography, Spacing } from '~/styles';
 import BackArrow from '~/assets/icons/back-arrow.svg';
+import Fav from '~/assets/icons/heart.svg';
+import FavSelected from '~/assets/icons/heart-solid.svg';
 import { ICON_DIMENSIONS } from '~/utils/constants';
 import { navigationPropTypes } from '~/types';
 
@@ -18,11 +20,11 @@ const styles = StyleSheet.create({
     height: 175,
     borderRadius: 10,
     alignSelf: 'center',
-    top: -50,
+    top: -100,
   },
   name: {
     fontFamily: 'lora-bold',
-    marginTop: Spacing.small,
+    marginTop: Spacing.large,
     textAlign: 'center',
     fontSize: Typography.xlargeFontSize,
   },
@@ -44,30 +46,76 @@ const styles = StyleSheet.create({
     left: Spacing.small,
     top: Spacing.medium,
   },
+  fav: {
+    position: 'relative',
+    top: 6,
+    right: -6,
+  },
+  favBackground: {
+    position: 'relative',
+    alignSelf: 'flex-end',
+    top: -75,
+    right: Spacing.large,
+    backgroundColor: Colors.lightGray,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: Colors.lightGray,
+    borderRadius: 44 / 2,
+    elevation: 2,
+    width: 44,
+    height: 44,
+    zIndex: 1,
+  },
 });
 
-const RestaurantDetails = ({ navigation }) => (
-  <View>
-    <Image
-      style={styles.image}
-      source={require('~/assets/cuisine/mexican/mexican.jpg')}
-    />
-    <View style={styles.restaurantDetails}>
-      <Text style={styles.name}>El Takito</Text>
-      <Text style={styles.cuisine}>Mexican</Text>
-      <Text style={styles.vicinity}>Silicon Valley</Text>
-    </View>
-    <View style={styles.backButton}>
-      <TouchableOpacity onPress={navigation.goBack}>
-        <BackArrow
-          width={ICON_DIMENSIONS.width}
-          height={ICON_DIMENSIONS.height}
-          fill={Colors.white}
-        />
+const RestaurantDetails = ({ navigation }) => {
+  const [isFav, onFav] = useState(false);
+  const { width: iconWidth, height: iconHeight } = ICON_DIMENSIONS;
+
+  return (
+    <View>
+      <Image
+        style={styles.image}
+        source={require('~/assets/cuisine/mexican/mexican.jpg')}
+      />
+      <TouchableOpacity
+        style={styles.favBackground}
+        hitSlop={{ top: 50, left: 50, bottom: 50, right: 50 }}
+        activeOpacity={0.8}
+        onPress={() => onFav(!isFav)}
+      >
+        {isFav ? (
+          <FavSelected
+            style={styles.fav}
+            width={iconWidth}
+            height={iconHeight}
+            fill={Colors.red}
+          />
+        ) : (
+          <Fav
+            style={styles.fav}
+            width={iconWidth}
+            height={iconHeight}
+            fill={Colors.mediumGray}
+          />
+        )}
       </TouchableOpacity>
+      <View style={styles.restaurantDetails}>
+        <Text style={styles.name}>El Takito</Text>
+        <Text style={styles.cuisine}>Mexican</Text>
+        <Text style={styles.vicinity}>Silicon Valley</Text>
+      </View>
+      <View style={styles.backButton}>
+        <TouchableOpacity onPress={navigation.goBack}>
+          <BackArrow
+            width={ICON_DIMENSIONS.width}
+            height={ICON_DIMENSIONS.height}
+            fill={Colors.white}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
-  </View>
-);
+  );
+};
 
 RestaurantDetails.propTypes = {
   navigation: navigationPropTypes.isRequired,
--- src/styles/colors.js
+++ src/styles/colors.js
@@ -4,5 +4,6 @@ const white = '#ffffff';
 const lightBlue = '#3da9fc';
 const lightGray = '#f9f9f9';
 const mediumGray = '#b0b0b0';
+const red = '#ff0000';
 
-export { black, darkBlue, mediumGray, lightBlue, lightGray, white };
+export { black, darkBlue, mediumGray, lightBlue, lightGray, red, white };
